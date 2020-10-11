63c64e6ab772091b953cd7b10c7efbcdac867dc9
Sebastien Cheraux <scheraux@gmail.com>
2020-10-11 15:26:24 -0700

    prevent renouncing ownership

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -8,7 +8,7 @@ contract SharedWallet is Allowance {
     event MoneyReceived(address indexed _from, uint _amount);
 
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
-        require(address(this).balance >= _amount, "Not enough money in contract");
+        require(address(this).balance >= _amount, "Not enough money in this contract");
         reduceAllowance(_amount);
         emit MoneySent(_to, _amount);
         _to.transfer(_amount);
@@ -16,5 +16,9 @@ contract SharedWallet is Allowance {
 
     receive() external payable {
         emit MoneyReceived(msg.sender, msg.value);
+    }
+
+    function renounceOwnership() public override onlyOwner {
+        revert("Can't renounce ownership with this contract");
     }
 }
