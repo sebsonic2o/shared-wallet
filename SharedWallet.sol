49032555e3251e8830c9e01f5f484be9f67b7eb5
Sebastien Cheraux <scheraux@gmail.com>
2020-10-11 08:01:23 -0700

    leverage ownable from openzeppelin

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -1,16 +1,11 @@
 // SPDX-License-Identifier: Attribution CC BY 4.0
 pragma solidity >=0.6.0 <0.7.0;
 
-contract SharedWallet {
-    address owner;
-    
-    constructor() public {
-        owner = msg.sender;
-    }
-    
-    modifier onlyOwner() {
-        require(owner == msg.sender, "Not the owner");
-        _;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract SharedWallet is Ownable {
+    function isOwner() internal view returns(bool) {
+        return owner() == msg.sender;
     }
     
     function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
