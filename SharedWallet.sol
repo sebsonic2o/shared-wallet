9b431294247b7f88a780d421f51461fe4bb92509
Sebastien Cheraux <scheraux@gmail.com>
2020-10-11 09:20:44 -0700

    support allowance

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -7,11 +7,32 @@ contract SharedWallet is Ownable {
     function isOwner() internal view returns(bool) {
         return owner() == msg.sender;
     }
-    
-    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+
+    mapping(address => uint) public allowance;
+
+    function addAllowance(address _to, uint _amount) public onlyOwner {
+        if(_to != owner()) {
+            allowance[_to] += _amount;
+        }
+    }
+
+    modifier ownerOrAllowed(uint _amount) {
+        require(isOwner() || allowance[msg.sender] >= _amount, "Not allowed");
+        _;
+    }
+
+    function reduceAllowance(uint _amount) public ownerOrAllowed(_amount) {
+        if(!isOwner()) {
+            allowance[msg.sender] -= _amount;
+        }
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+        require(address(this).balance >= _amount, "Contract doesn't have enough funds");
+        reduceAllowance(_amount);
         _to.transfer(_amount);
     }
-    
+
     receive() external payable {
         
     }
