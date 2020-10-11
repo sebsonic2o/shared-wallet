0fe5ce2d0fd9f26812a16372cae72d1fbbddfa1d
Sebastien Cheraux <scheraux@gmail.com>
2020-10-11 12:00:30 -0700

    refactor isowner

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -1,34 +1,33 @@
-// SPDX-License-Identifier: Attribution CC BY 4.0
 pragma solidity >=0.6.0 <0.7.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract SharedWallet is Ownable {
-    function isOwner() internal view returns(bool) {
-        return owner() == msg.sender;
+    function isOwner(address _who) internal view returns(bool) {
+        return owner() == _who;
     }
 
     mapping(address => uint) public allowance;
 
     function addAllowance(address _to, uint _amount) public onlyOwner {
-        if(_to != owner()) {
+        if(!isOwner(_to)) {
             allowance[_to] += _amount;
         }
     }
 
     modifier ownerOrAllowed(uint _amount) {
-        require(isOwner() || allowance[msg.sender] >= _amount, "Not allowed");
+        require(isOwner(msg.sender) || allowance[msg.sender] >= _amount, "Not allowed");
         _;
     }
 
     function reduceAllowance(uint _amount) public ownerOrAllowed(_amount) {
-        if(!isOwner()) {
+        if(!isOwner(msg.sender)) {
             allowance[msg.sender] -= _amount;
         }
     }
 
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
-        require(address(this).balance >= _amount, "Contract doesn't have enough funds");
+        require(address(this).balance >= _amount, "Contract doesn't have enough money");
         reduceAllowance(_amount);
         _to.transfer(_amount);
     }
