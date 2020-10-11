aae1e76b6e0671db9ed3a14342bfd5291877f305
Sebastien Cheraux <scheraux@gmail.com>
2020-10-11 14:58:32 -0700

    add money events

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -4,13 +4,17 @@ pragma solidity >=0.6.0 <0.7.0;
 import "./Allowance.sol";
 
 contract SharedWallet is Allowance {
+    event MoneySent(address indexed _to, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
+
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
         require(address(this).balance >= _amount, "Not enough money in contract");
         reduceAllowance(_amount);
+        emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
 
     receive() external payable {
-        
+        emit MoneyReceived(msg.sender, msg.value);
     }
 }
